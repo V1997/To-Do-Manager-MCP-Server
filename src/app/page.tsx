8df666004317ac82f1c2ce7261ec9531bd404f42
Vasu Patel <patelvasu1997@gmail.com>
2025-07-05 18:06:06 -0500

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Test MCP server</h1>
+      <p>This is a test MCP server</p>
+      <p>This is a test MCP server</p>  
+     </div>
+  );
+}
